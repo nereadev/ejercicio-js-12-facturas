4e2a36045684bb5b003f614337941ed95a75c35f
Nerea García <nereagarcia.c@gmail.com>
2021-03-04 16:52:36 +0100

    Añadir todas las filas tipo ingreso

--- js/script.js
+++ js/script.js
@@ -3,18 +3,31 @@ devolverFactuas();
 async function devolverFactuas() {
   const facturas = await fetch("http://localhost:3001/facturas");
   const listaFacturas = await facturas.json();
-  console.log(listaFacturas);
   // añadir factura tipo ingreso en fila
-  listaMolde.classList.remove("off");
-  const tipoIngreso = listaFacturas
+  const tipoIngreso = await listaFacturas
     .filter(factura => factura.tipo === "ingreso");
-  listaMolde.textContent = tipoIngreso[0].base;
-  document.querySelector(".lista-facturas").append(listaMolde);
-  console.log(listaMolde);
+  console.log(tipoIngreso);
+  for (const factura of await tipoIngreso) {
+    const nuevaFila = listaMolde.cloneNode(true);
+    nuevaFila.classList.remove("off");
+    nuevaFila.querySelector(".numero").textContent = factura.id;
+    nuevaFila.querySelector(".concepto").textContent = factura.concepto;
+    console.log(factura.concepto);
+    nuevaFila.querySelector(".fecha").textContent = factura.fecha;
+    nuevaFila.querySelector(".base").textContent = factura.base;
+    nuevaFila.querySelector(".iva").textContent = factura.tipoIva;
+    nuevaFila.querySelector(".total").textContent = factura.numero;
+    nuevaFila.querySelector(".estado").textContent = factura.abonada;
+    nuevaFila.querySelector(".vence").textContent = factura.vencimiento;
+    document.querySelector(".lista-facturas").append(nuevaFila);
+  }
 }
 
 // crear fila "dummy"
-const listaMolde = document.querySelector(".lista-dummy").cloneNode(true);
-listaMolde.textContent = "";
+const listaMolde = document.querySelector(".lista-dummy");
+// listaMolde.textContent = "";
+// console.log(listaMolde);
 listaMolde.classList.add("off");
+
+document.querySelector(".lista-facturas").textContent = "";
 // console.log(listaMolde);
